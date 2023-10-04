86200d793c66af43bb36ba3a3bb658cf12900858
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2023-10-04 15:12:42 +0530

    Update Payments.vue

--- computroidpos/public/js/posapp/components/pos/Payments.vue
+++ computroidpos/public/js/posapp/components/pos/Payments.vue
@@ -639,7 +639,7 @@
           >
         </v-col>
         <v-col cols="6" class="pl-1">
-          <v-btn
+          <!-- <v-btn
             block
             large
             color="success"
@@ -647,7 +647,19 @@
             @click="submit(undefined, false, true)"
             :disabled="vaildatPayment"
              accesskey="s"
-            >{{ __("Submit & Print") }}(Alt + S)</v-btn
+            >{{ __("Submit & Print") }}(Alt + S)
+            </v-btn -->
+            <v-btn
+            block
+            large
+            color="success"
+            dark
+            @click="submit(undefined, false, true)"
+            :disabled="vaildatPayment"
+            id="submitPrintButton"
+          >
+            {{ __("Submit & Print") }}
+          </v-btn>
           >
         </v-col>
         <v-col cols="12">
@@ -1466,6 +1478,17 @@ export default {
         this.invoice_doc.sales_team = [];
       }
     },
+    // Add an event listener for the F9 key press
+    document.addEventListener('keydown', function(event) {
+    if (event.key === 'F9') {
+    // Trigger the click event on the button with id "submitPrintButton"
+      var button = document.getElementById('submitPrintButton');
+    if (button && !button.disabled) {
+      button.click();
+    }
+  }
+}),
   },
 };
 </script>
+
