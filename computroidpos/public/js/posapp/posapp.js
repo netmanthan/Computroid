81ed8ba7463b5502467a46165e4bd8cdf22ba865
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2023-10-04 12:44:57 +0530

    Update posapp.js

--- computroidpos/public/js/posapp/posapp.js
+++ computroidpos/public/js/posapp/posapp.js
@@ -45,5 +45,23 @@ frappe.PosApp.posapp = class {
     setup_header () {
 
     }
+    <script>
+// Function to simulate a click on the button with text "UPI"
+function clickUPIButton() {
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].textContent === "UPI") {
+            buttons[i].click();
+            break; // Stop searching after the first match
+        }
+    }
+}
+
+// Event listener for the F9 key press
+document.addEventListener("keydown", function(event) {
+    if (event.key === "F9") {
+        clickUPIButton();
+    }
+});
 
 };
