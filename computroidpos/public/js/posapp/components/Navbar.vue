96f9f245a32f6a0c5c15f307ec1ca7020d6546e6
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2023-10-06 00:50:18 +0530

    Update Navbar.vue

--- computroidpos/public/js/posapp/components/Navbar.vue
+++ computroidpos/public/js/posapp/components/Navbar.vue
@@ -119,6 +119,12 @@
             <v-list-item-content>
               <v-list-item-title v-text="item.text"></v-list-item-title>
             </v-list-item-content>
+            <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+            <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
